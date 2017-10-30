c5f0198736e59ab085427d88b99f395b7974680f
alexander <blackbirdeagle@mail.ru>
2017-10-30 12:25:32 +0300

    Create scroll for anchor

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
-$(document).ready(function() {
-    $('.prof-sites__service, .seo__service, .design__service, .other__service').click(function(){
-        $('.service-detail').animate({
-            right: '0',
-        }, 500, function(){
-            $('body').css({'overflow-y': 'hidden'});
-        });
+$(function(){
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
     });
-
-    function keyExit(e){
-        if(e.keyCode == 27){
-            $('.service-detail').animate({
-                    right: '-100%',
-                }, 500, function(){
-                $('body').css({'overflow': 'auto'});
-            });
-        }
-    }
-
-    addEventListener("keydown", keyExit);
 });
+
+$('.prof-sites__service, .seo__service, .design__service, .other__service').click(function(){
+    $('.service-detail').animate({
+        right: '0',
+    }, 500, function(){
+        $('body').css({'overflow-y': 'hidden'});
+    });
+});
+
+function keyExit(e){
+    if(e.keyCode == 27){
+        $('.service-detail').animate({
+                right: '-100%',
+            }, 500, function(){
+            $('body').css({'overflow': 'auto'});
+        });
+    }
+}
+
+addEventListener("keydown", keyExit);
